bc5c398fce0874a70059445980ed568fa53d0b62
IrisP25 <42620873+IrisP25@users.noreply.github.com>
2019-09-23 10:42:20 -0400

    Create MichelTriggerDecision.h

---
+++ MichelTriggerDecision.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int MichelTriggerDecision(vector<vector<int>> candidates);
