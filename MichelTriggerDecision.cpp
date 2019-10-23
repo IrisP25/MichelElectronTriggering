4cf9aa48a6929539fd72b33ff6ac3fc2ced10e09
IrisP25 <42620873+IrisP25@users.noreply.github.com>
2019-10-23 11:38:43 -0400

    Updated Michel Module code
    Added changes suggested by the group, added dqds, added low covariance calculations as well.

--- MichelTriggerDecision.cpp
+++ MichelTriggerDecision.cpp
@@ -1,88 +1,8 @@
-//#include "MichelTriggerDecision.h"
+#include "MichelTriggerModule.h"
+#include "MichelCalculationFunction.h"
 
-//int MichelTriggerDecision(vector<vector<int>> candidates){
-
-
-//some standard C++ includes
-#include <iostream>
-#include <stdlib.h>
-#include <string>
-#include <sstream>
-#include <vector>
-#include <algorithm>
-#include <iomanip>
-#include <fstream>
-
-//some ROOT includes
-#include "TInterpreter.h"
-#include "TROOT.h"
-#include "TH1F.h"
-#include "TH2F.h"
-#include "TH3D.h"
-#include "TFile.h"
-#include "TTree.h"
-#include "TCanvas.h"
-#include "TStyle.h"
-#include "TString.h"
-#include "TStopwatch.h"
-
-struct TriggerCandidate{
-    int adjacency;
-    int adcpeak;
-    int adcsum;
-    int tspan;
-    int first_ch;
-    int last_ch;
-    double tstart;
-    double first_time;
-    double last_time;
-    int apanum;
-};
-
-struct TP{
-  unsigned int channel;
-  unsigned int tstart;
-  unsigned int tspan;
-  unsigned int adcsum;
-  unsigned int adcpeak;
-  unsigned int flags;
-};
-
-void newMichelTrigger(){
+int MichelTriggerModule(std::vector<TriggerCandidate> candidates){
     //using namespace std;
-    
-    //get the actual TCs from the txt
-    TString inputname1 = "/Applications/textFiles/MichelTriggering/tcsorted_20adjacency.txt";
-    std::ifstream inputFile1;
-    inputFile1.open(inputname1);
-    int adjacency;
-    int adcpeak;
-    int adcsum;
-    int tspan;
-    int first_ch;
-    int last_ch;
-    double tstart;
-    double first_time;
-    double last_time;
-    int apanum;
-    std::vector<TriggerCandidate> candidates;
-    while (inputFile1 >> adjacency >> adcpeak >> adcsum >> tspan >> first_ch>> last_ch>> tstart >> first_time >> last_time >> apanum){
-        TriggerCandidate tc;
-        tc.adjacency = adjacency;
-        tc.adcpeak = adcpeak;
-        tc.adcsum = adcsum;
-        tc.tspan = tspan;
-        tc.first_ch = first_ch;
-        tc.last_ch = last_ch;
-        tc.tstart = tstart;
-        tc.first_time = first_time;
-        tc.last_time = last_time;
-        tc.apanum = apanum;
-        candidates.push_back(tc);
-    }
-    inputFile1.close();
-    
-
   int prev_channel = -9000;
   int prev_time = -9000;
   int nChannels = 480;
@@ -99,6 +19,7 @@ void newMichelTrigger(){
       //if (last_APA == candidates.at(i_candidate).at(8)){
         if(fabs(candidates.at(i_candidate).first_ch - prev_channel) <=1 ){
         //if(fabs(candidates.at(i_candidate).at(4)- prev_channel) <=1  ){
+           // Michel_candidates.push_back(i_candidate);
             michel_candidates_num++;
             //std::cout << " grouping possible trigger" <<std::endl;
             //std::cout << fabs(candidates.at(i_candidate).first_ch- prev_channel) << " Channel difference" << std::endl;
@@ -106,9 +27,7 @@ void newMichelTrigger(){
         else{
             //std::cout <<candidates.at(i_candidate).at(4)- prev_channel<< " Too big of a channel gap" << std::endl;
             //if the next TC candidates is not withing a wire, check if the previous ones were, and try to trigger
-            
             if (michel_candidates_num >0){
-            //save first_time of the first TC and last_time for the last TC
                 m_candidate.first = candidates.at(i_candidate-michel_candidates_num-1).first_time;
                 m_candidate.second = candidates.at(i_candidate-1).last_time;
                 Michel_candidates.push_back(m_candidate);
@@ -126,8 +45,7 @@ void newMichelTrigger(){
       
       else{
           if (michel_candidates_num >0){
-          //save first_time of the first TC and last_time for the last TC
-		            m_candidate.first = candidates.at(i_candidate-michel_candidates_num-1).first_time;
+                    m_candidate.first = candidates.at(i_candidate-michel_candidates_num-1).first_time;
                 m_candidate.second = candidates.at(i_candidate-1).last_time;
                 Michel_candidates.push_back(m_candidate);
                 //std::cout << "Added a several candidate trigger " << i_candidate-michel_candidates_num << ","<< i_candidate<<std::endl;
@@ -148,41 +66,64 @@ void newMichelTrigger(){
     }
   }//finish looping through the trigger candidates
 
-  //for(unsigned int i_mic
+    for( unsigned int michel_tc =0; michel_tc < 1; michel_tc++){
     //now look search through the TPs given their tstarts
-    const auto tmp1 = Michel_candidates.at(0).first;
-    const auto tmp2 = Michel_candidates.at(0).second;
+        const auto tmp1 = Michel_candidates.at(michel_tc).first;
+        const auto tmp2 = Michel_candidates.at(michel_tc).second;
     
-    //is this part doable for tpsets?
-    auto first_tp = std::find_if(tpset.tps.begin(),tpset.tps.end(),[tmp1](const TP& tp_1){return (double)tp_1.tstart == tmp1 ;});
-    auto last_tp = std::find_if(tpset.tps.begin(),tpset.tps.end(),[tmp2](const TP& tp_2){return (double)tp_2.tstart == tmp2 ;});
+        //std::cout <<std::setprecision(30)<<Michel_candidates.at(0).first <<std::endl;
+        //std::cout <<std::setprecision(30)<<Michel_candidates.at(0).second <<std::endl;
+        //std::cout << tmp2 << std::endl;
+        //auto first_tp = std::find_if(candidates.begin(),candidates.end(),[](const TP& tps){return tps.tstart == Michel_candidates.at(0).first ;});
+        auto first_tp = std::find_if(candidates.begin(),candidates.end(),[tmp1](const TP& tp_1){return (double)tp_1.tstart == tmp1 ;});
+        auto last_tp = std::find_if(candidates.begin(),candidates.end(),[tmp2](const TP& tp_2){return (double)tp_2.tstart == tmp2 ;});
 
-    //std::cout << first_tp -tpset.tps.begin() <<std::endl;
-    //std::cout << last_tp - tpset.tps.begin() << std::endl;
-    
-    //now we get the index of the first TP and the last TP from our possible decaying muon
-    int tp1_index = (int)(first_tp -tpset.tps.begin());
-    int tp2_index = (int)(last_tp - tpset.tps.begin());
+    //std::cout << first_tp -candidates.begin() <<std::endl;
+    //std::cout << last_tp - candidates.begin() << std::endl;
+        int tp1_index = (int)(first_tp -tpset.tps.begin());
+        int tp2_index = (int)(last_tp - tpset.tps.begin());
     //std::cout << tp1_index <<std::endl;
     //std::cout << tp2_index << std::endl;
     
-    std::vector<TP> trigger_hits;
-    trigger_hits.reserve(tp2_index - tp1_index +1 );
-    for (int i_tp = tp1_index; i_tp < tp2_index +1; i_tp++){
-        trigger_hits.emplace_back(tpset.tps[i_tp]);
-        //std::cout <<trigger_hits.at(trigger_hits.size()-1).first_time << " " << trigger_hits.size()<< std::endl;
-    }
-    //now we proceed to calculate linearity and dqds..
-    //i will add this 
-    
+        std::vector<TP> trigger_hits;
+        trigger_hits.reserve(tp2_index - tp1_index +1 );
+    //std::vector<TriggerCandidate> trigger_hits;
+        for (int i_tp = tp1_index; i_tp < tp2_index +1; i_tp++){
+            trigger_hits.emplace_back(tpset.tps[i_tp]);
+            //std::cout <<std::setprecision(30) <<trigger_hits.at(trigger_hits.size()-1).first_time << " " << trigger_hits.size()<< std::endl;
+        }
+        
+        //now we proceed to calculate linearity and dqds..
+        std::vector<size_t> ordered_TPs;
+        std::vector<double> average_TP_distance, average_TP_distance_neighbors;
+        double d_cutoff = 15;
+        
+        //this will order the TPs in
+        ProcessTPs(trigger_hits, ordered_TPs, average_TP_distance, average_TP_distance_neighbors, d_cutoff);
+        std::vector< double> truncated_mean, truncated_dqds,covariance, slope;
+        size_t edge_fix = 3;
+        int covariance_window = 11;
+        int n_window_size = 15;
+        double p_above = 0.25;
+        int window_cutoff = 3;
+        bool calculate_truncated = CalcTruncated(trigger_hits, average_TP_distance, truncated_mean, truncated_dqds, covariance, slope, edge_fix, covariance_windowm n_window_size, p_above,window_cutoff);
+        
+        //if calculate truncated worked, move on
+        if (calculate_truncated){
+            size_t maxDistance = 20;
+            int boundary = 0;
+            bool find_BoundaryFromTQMaxQ = BoundaryFromTQMaxQ(trigger_hits,truncated_mean,ordered_TPs,maxDistance,boundary);
+            //if boundary found, move on
+            if (find_BoundaryFromTQMaxQ)
+            {
+                bool lowCovinBoundary = RequireBoundaryInLowCov(trigger_hits,covariance,boundary,0.9);
+                if (lowCovinBoundary) return 0;
+                else return 1;
+            }
+            continue;
+            
+        }
+        continue;
+    }//
   //return 0;
 }
-# ifndef __CINT__
-int main(){
-    newMichelTrigger();
-    hello();
-    return 0;
-}
-
-#endif
-
